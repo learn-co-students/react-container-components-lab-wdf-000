959ee87b8476a32becb340cd2b00d02fa0db7d09
sami4thewin <samiomarmohammed@gmail.com>
2017-03-08 14:52:31 -0500

    Done

--- components/MovieReviews.js
+++ components/MovieReviews.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 const MovieReviews = ({reviews}) => (
   <div className="review-list">
-    {reviews.map(review => <h5 className="review">{review.display_title}</h5>)}
+    {reviews.map((review, i) => <h5 key={i} className="review">{review.display_title}</h5>)}
   </div>
 )
 
--- components/SearchableMovieReviewsContainer.js
+++ components/SearchableMovieReviewsContainer.js
@@ -5,11 +5,33 @@ const MovieReviews = require('./MovieReviews');
 
 class SearchableMovieReviewsContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      reviews: [],
+      query: ''
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(ev) {
+    this.setstate({
+      reviews: this.state.reviews,
+      query: ev.target.value
+    })
+  }
+
+  componentWillMount() {
+   fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.query}?api-key=fc66b640b05549ee9af75b82e830cd28`)
+     .then((res => this.setState({ reviews: JSON.parse(res.results) })))
   }
 
   render() {
-    
+    return(
+      <div className="searchable-movie-reviews">
+        <input onChange={this.onChange}/>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
   }
 }
 
